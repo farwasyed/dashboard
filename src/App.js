38b73bf72d40ee0b2da40b26caf99e0d912500ad
farwasyed <farwa.asyed@gmail.com>
2023-02-07 23:53:53 +1100

    base layout - wip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import TopBar from './scenes/global/TopBar'
 
 export default function App() {
+  const [theme, ColorMode] = useMode();
+
   return (
-    <div>App</div>
+    <ColorModeContext.Provider value={ColorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+          <div className='app'>
+            <main className='content'>
+              <TopBar/>
+            </main>
+          </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
